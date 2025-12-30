1453011abc2603f0d7dc9cce7ba1a8f97a209c8a
Joel Wejdenstål <joel.wejdenstal@gmail.com>
2025-12-30 13:45:15 +0100

    mutwaker

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use std::task::Waker;
+
 use base64::Engine as _;
 use base64::engine::general_purpose;
 use rand::RngCore;
@@ -36,5 +38,57 @@ pub async fn unique_string(
         if !exists {
             return candidate;
         }
+    }
+}
+
+pub struct MutWaker {
+    registered: bool,
+    waker: Waker,
+}
+
+impl MutWaker {
+    pub fn new() -> Self {
+        Self {
+            registered: false,
+            waker: Waker::noop().clone(),
+        }
+    }
+
+    pub fn notify(&mut self) {
+        if self.registered {
+            self.registered = false;
+            self.waker.wake_by_ref();
+        }
+    }
+
+    pub fn notify_by_val(&mut self) {
+        if self.registered {
+            self.unregister_inner().wake();
+        }
+    }
+
+    pub fn register(&mut self, waker: &Waker) {
+        self.registered = true;
+        if self.waker.will_wake(waker) {
+            return;
+        }
+        // outlined to avoid a bunch of register fuss.
+        self.register_slow(waker);
+    }
+
+    #[cold]
+    #[inline(never)]
+    fn register_slow(&mut self, waker: &Waker) {
+        // using mem::replace instead of assignment seems to produce much more optimal assembly.
+        _ = std::mem::replace(&mut self.waker, waker.clone());
+    }
+
+    pub fn unregister(&mut self) {
+        drop(self.unregister_inner());
+    }
+
+    fn unregister_inner(&mut self) -> Waker {
+        self.registered = false;
+        std::mem::replace(&mut self.waker, Waker::noop().clone())
     }
 }
